c42131feb5e4c4938c326c3af4693b84b66d2983
Gajos <thegajos@gmail.com>
2021-01-21 15:17:31 +0100

    update

--- Framework/Inventory/Inventory.ts
+++ Framework/Inventory/Inventory.ts
@@ -1,1 +1,8 @@
-//
+/** @noSelfInFile */
+
+const { floor, pow, rad, deg, sin, cos, atan2, abs, pi } = math
+
+export class Inventory {
+
+    // constructor() {}
+}
--- Framework/Player/PlayerManager.ts
+++
@@ -1,9 +0,0 @@
-/** @noSelfInFile */
-
-import { Player } from './Player'
-
-class PlayerManager {
-    public static onJoin(id: PlayerID) {
-        // const user = new User(id)
-    }
-}
